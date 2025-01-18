905ba416c2b40b40aa90b808dffbafd06af7f482
Michael Bullut <michael.bullut@solstice.co.ke>
2025-01-18 14:41:44 +0300

    Add files via upload

---
+++ Installing MongoDB (Ubuntu 22.04).sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+apt update -y # Refreshing the system repository 
+wget -qO - https://www.mongodb.org/static/pgp/server-8.0.asc | sudo apt-key add - # Latest MongoDB Release Key (Can be adjusted for any desired version) 
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list 
+apt update -y # Refreshing the system repository again 
+apt install mongodb-org -y # Install MongoDB 
+systemctl enable --now mongod 
+mongod --version # Verify the MongoDB installation 
+# end of script 
