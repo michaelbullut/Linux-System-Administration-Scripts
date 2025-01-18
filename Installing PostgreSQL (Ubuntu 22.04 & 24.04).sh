b6bf0ac1d61d6ca2f134b6c32b298f19c20ec49b
Michael Bullut <michael.bullut@solstice.co.ke>
2025-01-18 21:54:15 +0300

    Update Installing PostgreSQL (Ubuntu 22.04 & 24.04).sh
    Added UFW rule.

--- Installing PostgreSQL (Ubuntu 22.04 & 24.04).sh
+++ Installing PostgreSQL (Ubuntu 22.04 & 24.04).sh
@@ -4,5 +4,6 @@ sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgd
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add - # Add Official Repository Signing Key 
 apt update -y # Refreshing the system repository again 
 apt install postgresql postgresql-contrib -y # Install PostgreSQL 
+ufw allow 5432/tcp # Allow for remote access 
 psql -V # Verify the PostgreSQL installation 
 # end of script 
