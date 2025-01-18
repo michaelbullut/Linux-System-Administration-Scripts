9ecda567ccec6a814fe9553c34cac57db70f23cd
Michael Bullut <michael.bullut@solstice.co.ke>
2025-01-18 16:05:12 +0300

    Add files via upload

---
+++ Installing PostgreSQL (Ubuntu 22.04 & 24.04).sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+apt update -y # Refreshing the system repository 
+sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list' # Add Official PostgreSQL Repository 
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add - # Add Official Repository Signing Key 
+apt update -y # Refreshing the system repository again 
+apt install postgresql postgresql-contrib -y # Install PostgreSQL 
+psql -V # Verify the PostgreSQL installation 
+# end of script 
