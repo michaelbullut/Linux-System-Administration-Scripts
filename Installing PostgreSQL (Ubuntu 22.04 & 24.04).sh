124997b4085c38443c9e49f0c4a848392fedce2f
Michael Bullut <michael.bullut@solstice.co.ke>
2026-05-09 20:31:39 +0300

    Update PostgreSQL installation script for key handling

--- Installing PostgreSQL (Ubuntu 22.04 & 24.04).sh
+++ Installing PostgreSQL (Ubuntu 22.04 & 24.04).sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 apt update -y # Refreshing the system repository 
-sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list' # Add Official PostgreSQL Repository 
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add - # Add Official Repository Signing Key 
+curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo gpg --dearmor -o /usr/share/keyrings/postgresql-keyring.gpg # Add Official Repository Signing Key 
+echo "deb [signed-by=/usr/share/keyrings/postgresql-keyring.gpg] http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list # Add Official PostgreSQL Repository 
 apt update -y # Refreshing the system repository again 
 apt install postgresql postgresql-contrib -y # Install PostgreSQL 
 ufw allow 5432/tcp # Allow for remote access (optional)
