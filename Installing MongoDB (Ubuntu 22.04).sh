f443c46cb6108c5c421732d47ba1ef29d5336f3b
Michael Bullut <michael.bullut@solstice.co.ke>
2026-05-09 21:45:19 +0300

    Update MongoDB installation script for key handling

--- Installing MongoDB (Ubuntu 22.04).sh
+++ Installing MongoDB (Ubuntu 22.04).sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 apt update -y # Refreshing the system repository 
-wget -qO - https://www.mongodb.org/static/pgp/server-8.0.asc | sudo apt-key add - # Latest MongoDB Release Key (Can be adjusted for any desired version) 
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list 
+curl -fsSL https://pgp.mongodb.com/server-8.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg --dearmor # Latest MongoDB Release Key (Can be adjusted for any desired version) 
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list 
 apt update -y # Refreshing the system repository again 
 apt install mongodb-org -y # Install MongoDB 
 systemctl enable --now mongod 
